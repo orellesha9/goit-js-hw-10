e998e1bd082101941343e0e253f7cee0c54e44aa
orellesha9 <orellesha9@gmail.com>
2023-11-20 00:32:04 +0200

    finish_poject1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import { fetchBreeds } from './cat-api'
+import { fetchBreeds } from './cat-api';
 import SlimSelect from 'slim-select';
-import { fetchCatByBreed } from './cat-api'
+import { fetchCatByBreed } from './cat-api';
 import axios from 'axios';
 import Notiflix from 'notiflix';
+
 refs = {
   selecter: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
@@ -14,14 +15,13 @@ refs = {
 //   select: '.breed-select',
 // });
 
-
 console.log(refs.catInfo);
 refs.loader.style.display = 'block';
 fetchBreeds()
-    .then(data => {
-      refs.selecter.innerHTML = data;
-      refs.loader.style.display = 'none';
-      refs.selecter.style.display = 'block';
+  .then(data => {
+    refs.selecter.innerHTML = data;
+    refs.loader.style.display = 'none';
+    refs.selecter.style.display = 'block';
   })
   .catch(err => {
     console.log(err);
@@ -33,24 +33,22 @@ fetchBreeds()
 
 refs.selecter.addEventListener('change', selecterEvent);
 
-
 function selecterEvent(event) {
   refs.catInfo.style.display = 'none';
-   refs.loader.style.display = 'block';
-     const selectedValue = event.target.value;
-    fetchCatByBreed(selectedValue)
-      .then(data => {
-        console.log(data);
-        refs.loader.style.display = 'none';
-        refs.catInfo.innerHTML = data;
-        refs.catInfo.style.display = 'flex';
-      })
-      .catch(err => {
-        Notiflix.Notify.failure(
-          'Oops! Something went wrong! Try reloading the page!'
-        );
-        refs.loader.style.display = 'none';
-        console.log(err);
-      });
+  refs.loader.style.display = 'block';
+  const selectedValue = event.target.value;
+  fetchCatByBreed(selectedValue)
+    .then(data => {
+      console.log(data);
+      refs.loader.style.display = 'none';
+      refs.catInfo.innerHTML = data;
+      refs.catInfo.style.display = 'flex';
+    })
+    .catch(err => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+      refs.loader.style.display = 'none';
+      console.log(err);
+    });
 }
-
